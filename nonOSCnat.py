f010e2228ef07b4bb2dfa9cf0d762d5ec03e4ebc
Wargreen <wargreen@lebib.org>
2017-02-02 07:10:11 +0100

    thread & gui rework

--- nonOSCnat.py
+++ nonOSCnat.py
@@ -15,25 +15,31 @@ from imports import nsmclient
 from imports.pyNonPeer import OurNonClient
 
 
+appTitle = "Non-OSC-NAT"
+
+
 #QT thead worker
 class NsmClientWorker(QObject):
     """Worker for NsmClient, maybe with his own eventloop"""
     
     nsmReadySign = QtCore.pyqtSignal(int)
     ourNsmClientSign= QtCore.pyqtSignal(object)
+    
+    openSessSign = QtCore.pyqtSignal(str, str)
+    saveSessSign = QtCore.pyqtSignal(str, str)
     exitAppSign = QtCore.pyqtSignal()
     
-    def __init__(self, appTitle, thread=None):
+    def __init__(self):
         QObject.__init__(self)
         qDebug("Start Init")
         
-        if thread is None:
-            thread = QThread()
-            self.moveToThread(thread)
-        """ NSM stuffs """        
+#        if thread is None:
+#            thread = QThread()
+#            self.moveToThread(thread)
 
-           
-           #All capabilities default to False. Just change one value to True if you program can do that.
+
+        """ NSM stuffs """        
+        #All capabilities default to False. Just change one value to True if you program can do that.
         capabilities = {
             "switch" : False,		#client is capable of responding to multiple `open` messages without restarting
             "dirty" : False, 		#client knows when it has unsaved changes
@@ -56,49 +62,54 @@ class NsmClientWorker(QObject):
         self.ourNsmClient, self.process = nsmclient.init(prettyName = appTitle, capabilities = capabilities, requiredFunctions = requiredFunctions, optionalFunctions = optionalFunctions,  sleepValueMs = 0) 
         self.ourNsmClientSign.emit(self.ourNsmClient)
         
-        thread.started.connect(lambda:self.run(thread))
-        thread.start()
+#        thread.started.connect(lambda:self.run(thread))
+#        thread.start()
         
         qDebug("Init Finished")
 
 
     def saveCallback(self,pathBeginning, clientId):
-        pass
-        return True, ""
-        
+        qDebug("Got a SaveCallback")
+        return False, "Not realy implemented"
+                
     def openOrNewCallback(self,pathBeginning, clientId):
-        return True, ""
+        self.openSessSign.emit(pathBeginning, clientId)
+        return True, "Not realy implemented"
 
     def exitCallback(self):
         self.exitAppSign.emit()
         return True
 
-    isRun = False
-    def run(self, thread):
-        if self.isRun:
-            qDebug("Running")
-        else:
-            self.isRun = True
-            qDebug("Worker start in progress")
+    def run(self, thread=None):
+#        self.isRun = False
+#        if self.isRun:
+#            qDebug("Running")
+#        else:
+#            self.isRun = True
+#            qDebug("Worker start in progress")
         
+        qDebug("Enter in event loop")
+        #thread.exec()
+        self.eventLoop = QtCore.QTimer()
+        self.eventLoop.start(500) #10ms-20ms is smooth for "real time" feeling. 100ms is still ok.
+        self.eventLoop.timeout.connect(self.rolling)
+
+    def rolling(self):
+        qDebug("Looped !")
         self.process()
         self.ourNsmClientSign.emit(self.ourNsmClient)
-        qDebug("Enter in event loop")
-        thread.exec()
 
 
 
-### QT main
-class Main(QtWidgets.QMainWindow):
+### GUI class
+class Gui(QtWidgets.QMainWindow):
     def __init__(self, qtApp):
+        
         super().__init__()
-        
-        appTitle = "Non-OSC-NAT"
-        
-        #self.window = QtWidgets.QMainWindow(self)
-        
+
         self.qtApp = qtApp
         self.setGeometry(300,300,350,250)
+        
         # Define all GUI elements
         self.mainLayout = QtWidgets.QVBoxLayout()
         self.wraperBox = QtWidgets.QGroupBox()
@@ -117,13 +128,15 @@ class Main(QtWidgets.QMainWindow):
         #self.spacer = QtWidgets.QSpacerItem()
         
         self.status = QtWidgets.QMainWindow.statusBar(self)
-        self.status.showMessage('Init...')
+        self.status.showMessage('Init...')  
         
         # Define QT's main widget
         self.mainWidget = QtWidgets.QWidget(self)
         self.mainWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.mainWidget)
         
+        
+    def showPeersSel(self):
         # Define elements places
         #self.setLayout(self.wraper)
         self.mainLayout.addWidget(self.wraperBox)
@@ -137,34 +150,47 @@ class Main(QtWidgets.QMainWindow):
         self.peerSelect.addWidget(self.listSelPeer)
         # Define widgets in peerPortForm
         self.peerPortForm.addWidget(self.peerPort)
-        self.peerPortForm.addWidget(self.peerPortApply)
+        self.peerPortForm.addWidget(self.peerPortApply)     
         
-      
+    def testGui(self):
+        self.status.addPermanentWidget(QtWidgets.QLabel(self.ourNsmClient.states.clientId))
+   
+     
+     
+
+### QT main
+class Main(QObject):
+    def __init__(self, qtApp, gui):
         
+        self.qtApp = qtApp
+        self.gui = gui
         # Try to thead
+        self.nsmClientThread = QThread()
+        self.nsmClientWorker = NsmClientWorker()
+        self.nsmClientWorker.moveToThread(self.nsmClientThread)
         
-        self.threads = []
-        nsmClientWorker = NsmClientWorker(appTitle)
+        self.nsmClientWorker.nsmReadySign.connect(self.nsmClientRdy)
+        self.nsmClientWorker.exitAppSign.connect(self.quit)
+        self.nsmClientWorker.ourNsmClientSign.connect(self.nsmClientGet)
+        self.nsmClientWorker.openSessSign.connect(gui.showPeersSel)
         
-        nsmClientWorker.nsmReadySign.connect(self.nsmClientRdy)
-        nsmClientWorker.exitAppSign.connect(self.quit)
-        nsmClientWorker.ourNsmClientSign.connect(self.nsmClientGet)
-        self.threads.append(nsmClientWorker)
-        #nsmClientWorker.start()
+        self.nsmClientThread.started.connect(self.nsmClientWorker.run)
+        
+        self.nsmClientThread.start()
+        self.nsmClientThread.exec()
         qDebug("Worker in place, running...")
         
-        self.eventLoop = QtCore.QTimer()
-        self.eventLoop.start(100) #10ms-20ms is smooth for "real time" feeling. 100ms is still ok.
+        
 
-    
-    
+
     def nsmClientRdy(self, loops):
+        
         #self.title.setText("<b>" + str(loops) + "</b>")
         pass
     
     def nsmClientGet(self, ourNsmClient):
         self.ourNsmClient = ourNsmClient
-        self.testGui()
+        gui.testGui()
     
     def applyPort(self):
         pass
@@ -174,16 +200,15 @@ class Main(QtWidgets.QMainWindow):
         self.title.setText("Quitte")
         exit()
 
-    def testGui(self):
-        self.status.addPermanentWidget(QtWidgets.QLabel(self.ourNsmClient.states.clientId))
         #self.title.setText(str(self.ourNsmClient.states.prettyNSMName))
 
 
 if __name__ == '__main__':
             
     qtApp = QtWidgets.QApplication(sys.argv)
-    nsmOSC = Main(qtApp)
-    nsmOSC.show()
+    nonNatUi = Gui(qtApp)
+    nonNatUi.show()
+    nonNat = Main(qtApp, nonNatUi)
 
     ##################
     #Start everything
