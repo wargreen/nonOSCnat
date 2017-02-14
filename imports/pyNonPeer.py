82cead16a33359449ab0742e54ad1f11f404eacf
Wargreen <wargreen@lebib.org>
2017-02-14 02:05:53 +0100

    First working version.

--- imports/pyNonPeer.py
+++ imports/pyNonPeer.py
@@ -1,57 +1,235 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-version = 0.1
+from imports.callback_dict import CallbackDict
+import liblo
+
+version = "0.1.1"
+
+"""
+    callbacks should be a dict with the form :
+        dict{
+            addpeer: addPeerCallback, 
+            delpeer: delPeerCallback, 
+            addsignal: addSignalCallback, 
+            delsignal: delSignalCallback
+        }
+"""
+nonCallbacks = {"addpeer" : None,
+                "delpeer" : None}
+
+nonParms = {"baseport" : 16000,}
+
+
+
 
 class NonPeer(object):
     """
     Representation of a non-* software, and stock of his controls
     """
-    def __init__(self, url, name, version, ident):
-        self.url = url
-        self.name = name
-        self.version = version
-        self.ident = ident
+    
+    def __init__(self, url, name=None, version=None, clientId=None):
+        self._url = url
+        self._name = name
+        self._version = version
+        self._clientId = clientId
+        self._extPort = 0
+        self._oscServer = None
+        self._oscIncome = False
+         
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def clientId(self):
+        return str(self._clientId)
+
+    @property
+    def extPort(self):
+        return int(self._extPort)
+
+    @extPort.setter
+    def extPort(self, port):
+        if port != self._extPort:
+            self._oscServer = liblo.Server(port)
+            self._oscServer.add_method(None, None, self.oscForward)
+            self._extPort = port
+            #print("Assigned port " + str(port) + " to peer " + self._clientId)
+    
+    @property
+    def ocsIncome(self):
+        return self._oscIncome
+    
+    def oscForward(self, path, argList, types): 
+        path = self._clientId + path
+        #print("Got " + path + " fw to " + self._url)
+        i = 0
+        m = liblo.Message(path)
+        #print(str(path) + " arguments :")
+        for a in argList:
+            m.add((types[i], a))
+            #print(str(types[i]) + " " + str(a))
+            i += 1
+        self._oscServer.send(self._url, m)
         
+    def procOsc(self):
+        oscIncome = self._oscServer.recv(0)
+        if oscIncome != self._oscIncome:
+            self._oscIncome = oscIncome
+            return oscIncome
         
+    
+
+# External port of a new peer
+newExtPort = nonParms["baseport"]
+
+class NonPeers(dict):
+    """
+    Custom dict for stock all peers instances
+    It call alls neededs callbacks when add and delete peer
+    """
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+#    def __getitem__(self, key):
+#        if self.get_callback:
+#            self.get_callback()
+#        return dict.__getitem__(self, key)
+
+
+    # For a new peer : peerId = clientId, val = url
+    def __setitem__(self, peerId, *arg, **kargs):
+    
+
+        if peerId not in self:
+            
+            # New peer instance
+            dict.__setitem__(self, peerId, NonPeer(arg, kargs, clientId = peerId))
+            
+            # port assignation
+            global newExtPort
+            self[peerId].extPort = newExtPort
+            print("Got hello from : " + peerId)
+            newExtPort += 1
+            
+            if nonCallbacks["addpeer"]:
+                nonCallbacks["addpeer"](self[peerId])
+
+    def __delitem__(self, peerId):
+        if self.del_callback:
+            self.del_callback()
+        dict.__delitem__(self, key)
+
 
 class OurNonClient(object):
-    
-    # OSC::Signal messages from https://github.com/original-male/non/blob/master/nonlib/OSC/Endpoint.C#L216
-    # All will be implemented... One Day...
-    methods = {"/non/hello" : ("ssss", None), # Broadcasted Hello message. (osc_url, app_title, version, instance_name)
-               "/signal/hello" : ("ss", None), # Unicast Hello response from app. (Ident, url)
-               "/signal/connect" : ("ss", None), # Inform about a new control link. (source Ident/path, dest Ident/path)
-               "/signal/disconnect" : ("ss", None), # Inform about a removed control link. (source Ident/path, dest Ident/path)
-               "/signal/renamed" : ("ss", None), # TODO
-               "/signal/removed" : ("s", None), # TODO
-               "/signal/created" : ("ssfff", None), # Inform about a new controlable. (Ident/path, [in|out], min, max, default)
-               "/signal/list" : ("", None), # Ask for possible controls. (reply form = path : /reply. "/signal/list", Ident/path, [in|out], min, max, default)
-               #"/reply" : ("", None), # TODO
-               }
-    # Dict of all peers as Id : nonPeerObj
-    nonPeers = {}               
-    
-    def __init__(self, nsmClient):
-        nsmClient = nsmClient
-        self.nsmStates = nsmClient.states
+    """
+        The non-* client itself.
+        Register methods to libloServer, discover & store peers.
+    """
+    def __init__(self, nsmClient, callbacks, parms):
+
         
-        nsmClient.methodAdder(self.methods)
-        self.nonPeers[self.nsmStates.ourUrl] = NonPeer(self.nsmtates.ourUrl, version, self.nsmStates.clientId)
+        # Methods to pass to liblo server.
+        # Form : "/osc/path" : (Callback, Reply Callback)    
+        # OSC::Signal messages from https://github.com/original-male/non/blob/master/nonlib/OSC/Endpoint.C#L216
+        # All will be implemented... One Day...
+
+        # Without args types, better intergration with nsmclient.py
+        methods = {"/non/hello" : (self.handleHello, None), # Broadcasted Hello message. (osc_url, app_title, version, instance_name)
+                   "/signal/hello" : (self.handleHello, None), # Unicast Hello response from app. (Ident, url)
+                   "/signal/connect" : (None, None), # Inform about a new control link. (source Ident/path, dest Ident/path)
+                   "/signal/disconnect" : (None, None), # Inform about a removed control link. (source Ident/path, dest Ident/path)
+                   "/signal/renamed" : (None, None), # TODO
+                   "/signal/removed" : (None, None), # TODO
+                   "/signal/created" : (None, None), # Inform about a new controlable. (Ident/path, [in|out], min, max, default)
+                   "/signal/list" : (None, None), # Ask for possible controls. (reply form = path : /reply. "/signal/list", Ident/path, [in|out], min, max, default)
+                   #"/reply" : ("", None), # TODO
+                   }
+
+        
+    #    # With or without types ??? This is the question...
+    #    
+    #    methods = {"/non/hello" : ("sssss", None), # Broadcasted Hello message. ("/non/hello", osc_url, app_title, version, instance_name)
+    #               "/signal/hello" : ("ss", None), # Unicast Hello response from app. (Ident, url)
+    #               "/signal/connect" : ("ss", None), # Inform about a new control link. (source Ident/path, dest Ident/path)
+    #               "/signal/disconnect" : ("ss", None), # Inform about a removed control link. (source Ident/path, dest Ident/path)
+    #               "/signal/renamed" : ("ss", None), # TODO
+    #               "/signal/removed" : ("s", None), # TODO
+    #               "/signal/created" : ("ssfff", None), # Inform about a new controlable. (Ident/path, [in|out], min, max, default)
+    #               "/signal/list" : ("", None), # Ask for possible controls. (reply form = path : /reply. "/signal/list", Ident/path, [in|out], min, max, default)
+    #               #"/reply" : ("", None), # TODO
+    #               }
+        
+        # redefines globals non callbacks
+        global nonCallbacks
+        nonCallbacks = callbacks
+
+        # 
+        global nonParms
+        nonParms = parms
+        
+        self.nsmClient = nsmClient
+        self.nsmStates = self.nsmClient.states
+        self.libloServer = nsmClient.libloServer
+        
+        
+        nsmClient.methodAdder(methods)
+    
+    def loadSession(self):
+        
+        # New peers list instance
+        self.nonPeers = NonPeers()
+        #self.nonPeers.clear()
+        self.nonPeers[self.nsmStates.clientId] = self.nsmStates.ourUrl,self.nsmStates.prettyNSMName, version, self.nsmStates.clientId
+        self.sayNonHello()
+        
         
     
-    def sayHello(self):
+    def sayNonHello(self):
         """
         Send a broadcast bundle to NSM,
          url : /nsm/server/broadcast
          format : sssss
          "/non/hello", osc_url, app_title, version, instance_name
         """
-        pass
+        msg = liblo.Message("/nsm/server/broadcast")
+        msg.add("/non/hello")
+        msg.add(str(self.nsmStates.ourUrl))
+        msg.add(str(self.nsmStates.prettyNSMName))
+        msg.add(str(version))
+        msg.add(str(self.nsmStates.clientId))
+        bndl = liblo.Bundle(msg)
         
-    def handleHello(self):
+        self.libloServer.send(self.nsmStates.nsmUrl, bndl)
+            
+    def handleHello(self, path, argList, types):
         """
         Create a NonPeer instance
 
         """
-        pass
+        print(path)
+        if path == "/signal/hello":
+            peerId, url = argList
+            self.nonPeers[peerId] = url
+        
+        elif path == "/non/hello":
+            url, name, version, peerId = argList
+            self.nonPeers[peerId] = url, {"name" : name,
+                                         "version" : version} 
+        
+    def process(self):
+        """
+        we need to call server.recv on all liblo instances
+        """
+        for peerId, peerObj in self.nonPeers.items():
+            peerObj.procOsc()
+
+        
+        
+        
+        
+        
+        
--- nonOSCnat.py
+++ nonOSCnat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Based on pynsmclient2 by Nils Gey.
+"""Based on pynsmclient by Nils Gey.
 The goal : list all NSM clients and route externals OSC messages to it.
 ... good luck to myself
 """
@@ -10,9 +10,11 @@ import sys
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import qDebug, QObject, QThread
 import logging
+import time
 
 from imports import nsmclient
 from imports.pyNonPeer import OurNonClient
+from imports.sighandler import SignalWakeupHandler
 
 
 appTitle = "Non-OSC-NAT"
@@ -21,13 +23,16 @@ appTitle = "Non-OSC-NAT"
 #QT thead worker
 class NsmClientWorker(QObject):
     """Worker for NsmClient, maybe with his own eventloop"""
-    
+    ### NSM signals ###
     nsmReadySign = QtCore.pyqtSignal(int)
     ourNsmClientSign= QtCore.pyqtSignal(object)
     
     openSessSign = QtCore.pyqtSignal(str, str)
     saveSessSign = QtCore.pyqtSignal(str, str)
     exitAppSign = QtCore.pyqtSignal()
+    
+    ### NON signals ###
+    updateListSign = QtCore.pyqtSignal(str, object)
     
     def __init__(self):
         QObject.__init__(self)
@@ -45,12 +50,12 @@ class NsmClientWorker(QObject):
             "dirty" : False, 		#client knows when it has unsaved changes
             "progress" : False,		#client can send progress updates during time-consuming operations
             "message" : False, 		#client can send textual status updates
-            "optional-gui" : False,	#client has an optional GUI	
+            "optional-gui" : False,	#client has an optional GUI
             }
 
         requiredFunctions = {
             "function_open" : self.openOrNewCallback, #Accept two parameters. Return two values. A bool and a status string. Otherwise you'll get a message that does not help at all: "Exception TypeError: "'NoneType' object is not iterable" in 'liblo._callback' ignored"
-            "function_save" : self.saveCallback, #Accept one parameter. Return two values. A bool and a status string. Otherwise you'll get a message that does not help at all: "Exception TypeError: "'NoneType' object is not iterable" in 'liblo._callback' ignored"					
+            "function_save" : self.saveCallback, #Accept one parameter. Return two values. A bool and a status string. Otherwise you'll get a message that does not help at all: "Exception TypeError: "'NoneType' object is not iterable" in 'liblo._callback' ignored"
             }
 
         optionalFunctions = {
@@ -59,44 +64,81 @@ class NsmClientWorker(QObject):
             "function_hideGui" : None, #Accept zero parameters. Return True or False
             "function_sessionIsLoaded" : None, #No return value needed.
             }
-        self.ourNsmClient, self.process = nsmclient.init(prettyName = appTitle, capabilities = capabilities, requiredFunctions = requiredFunctions, optionalFunctions = optionalFunctions,  sleepValueMs = 0) 
+        """ 
+        Non-stuffs stuffs 
+        
+        callbacks should be a dict with the form :
+            dict{
+                addpeer: addPeerCallback, # retrun the added peer object.
+                delpeer: delPeerCallback, 
+                addsignal: addSignalCallback, 
+                delsignal: delSignalCallback
+            }
+        """
+
+        ## Displaced in OurNSMClient
+        #self.ourNonClient = OurNonClient(self.ourNsmClient, self.nonCallbacks)
+        self.ourNsmClient, self.nsmProcess = nsmclient.init(prettyName = appTitle, 
+                                                         capabilities = capabilities,
+                                                         requiredFunctions = requiredFunctions,
+                                                         optionalFunctions = optionalFunctions,
+                                                         sleepValueMs = 0
+                                                         ) 
         self.ourNsmClientSign.emit(self.ourNsmClient)
         
-#        thread.started.connect(lambda:self.run(thread))
-#        thread.start()
         
+        nonCallbacks = {
+            "addpeer" : self.addpeerCallback,
+            "delpeer" : self.delpeerCallback
+            }
+        
+        nonPeersParms = {
+            "baseport" : 16000, # Start port for externalized client. 16000 by default
+        }
+        
+        self.ourNonPeer = OurNonClient(self.ourNsmClient, nonCallbacks, nonPeersParms)
+                
         qDebug("Init Finished")
 
+    ### NSM callbacks ###
 
-    def saveCallback(self,pathBeginning, clientId):
+    def saveCallback(self,pathBeginning):
         qDebug("Got a SaveCallback")
-        return False, "Not realy implemented"
+        returnStr = "\n Not realy implemented \n" + pathBeginning
+        return False, returnStr
                 
     def openOrNewCallback(self,pathBeginning, clientId):
+        qDebug("Got a OpenOrNewCallback")
         self.openSessSign.emit(pathBeginning, clientId)
+        self.ourNonPeer.loadSession()
         return True, "Not realy implemented"
 
     def exitCallback(self):
-        self.exitAppSign.emit()
+        qDebug("Got a ExitCallback")
+        #self.quit()
+        exit()
         return True
-
-    def run(self, thread=None):
-#        self.isRun = False
-#        if self.isRun:
-#            qDebug("Running")
-#        else:
-#            self.isRun = True
-#            qDebug("Worker start in progress")
         
+    ### Non Callbacks ###
+    
+    def addpeerCallback(self, peer):
+        self.updateListSign.emit("add", peer)
+    
+    def delpeerCallback(self, peerId):
+        self.updateListSign.emit("del", peerId)
+    
+
+    def run(self):
+       
         qDebug("Enter in event loop")
-        #thread.exec()
         self.eventLoop = QtCore.QTimer()
-        self.eventLoop.start(500) #10ms-20ms is smooth for "real time" feeling. 100ms is still ok.
+        self.eventLoop.start(10) #10ms-20ms is smooth for "real time" feeling. 100ms is still ok.
         self.eventLoop.timeout.connect(self.rolling)
 
     def rolling(self):
-        qDebug("Looped !")
-        self.process()
+        #qDebug("Looped !")
+        self.nsmProcess()
+        self.ourNonPeer.process()
         self.ourNsmClientSign.emit(self.ourNsmClient)
 
 
@@ -109,6 +151,9 @@ class Gui(QtWidgets.QMainWindow):
 
         self.qtApp = qtApp
         self.setGeometry(300,300,350,250)
+        
+        # Define some attributs
+        self.timers = {}
         
         # Define all GUI elements
         self.mainLayout = QtWidgets.QVBoxLayout()
@@ -128,12 +173,16 @@ class Gui(QtWidgets.QMainWindow):
         #self.spacer = QtWidgets.QSpacerItem()
         
         self.status = QtWidgets.QMainWindow.statusBar(self)
-        self.status.showMessage('Init...')  
+        self.status.showMessage("Don't read that !'")  
         
         # Define QT's main widget
         self.mainWidget = QtWidgets.QWidget(self)
         self.mainWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.mainWidget)
+        
+        # Connect gui widgets to functions
+        self.listSelPeer.currentItemChanged.connect(self.selPeerItem)
+        self.peerPortApply.clicked.connect(self.applyPort)
         
         
     def showPeersSel(self):
@@ -150,10 +199,44 @@ class Gui(QtWidgets.QMainWindow):
         self.peerSelect.addWidget(self.listSelPeer)
         # Define widgets in peerPortForm
         self.peerPortForm.addWidget(self.peerPort)
-        self.peerPortForm.addWidget(self.peerPortApply)     
+        self.peerPortForm.addWidget(self.peerPortApply)
         
-    def testGui(self):
-        self.status.addPermanentWidget(QtWidgets.QLabel(self.ourNsmClient.states.clientId))
+    def displayStatus(self, msg):
+        self.status.showMessage(str(msg))
+        
+    def updatePeerList(self, action, peer):
+        if action == "add":
+            newPeerItem = QtWidgets.QListWidgetItem(peer.clientId)
+            newPeerItem.setData(QtCore.Qt.UserRole, peer)
+            #self.peerDict[newPeerItem] = peer
+            self.listSelPeer.addItem(newPeerItem)
+            self.displayStatus("Discovered new peer : " + peer.clientId)
+        elif action == "remove":
+            pass
+    
+    def selPeerItem(self, curr, prev):
+        nonPeer = curr.data(QtCore.Qt.UserRole)
+        print(nonPeer.extPort)
+        self.peerPort.setValue(nonPeer.extPort)
+    
+    def applyPort(self):
+        selPeerInList = self.listSelPeer.selectedItems()
+        selNonPeer = selPeerInList[0].data(QtCore.Qt.UserRole)
+        #selNonPeer = selPeerData.toPyObject()
+        selNonPeer.extPort = self.peerPort.value()
+    
+#    def displayIncomeOsc(self):
+#        all_items = self.listSelPeer.findItems('', QtCore.Qt.MatchRegExp)
+#        
+#        for item in all_items:
+#            peer = item.data(QtCore.Qt.UserRole)
+#            if peer.oscIncome
+    
+    
+    
+    
+#    def testGui(self):
+#        self.status.addPermanentWidget(QtWidgets.QLabel(self.ourNsmClient.states.clientId))
    
      
      
@@ -168,16 +251,27 @@ class Main(QObject):
         self.nsmClientThread = QThread()
         self.nsmClientWorker = NsmClientWorker()
         self.nsmClientWorker.moveToThread(self.nsmClientThread)
+        self.gui.displayStatus("Init...")
+        ## Connect alls signals
         
+        # NSM
         self.nsmClientWorker.nsmReadySign.connect(self.nsmClientRdy)
         self.nsmClientWorker.exitAppSign.connect(self.quit)
         self.nsmClientWorker.ourNsmClientSign.connect(self.nsmClientGet)
         self.nsmClientWorker.openSessSign.connect(gui.showPeersSel)
         
-        self.nsmClientThread.started.connect(self.nsmClientWorker.run)
+        # NON
+        self.nsmClientWorker.updateListSign.connect(gui.updatePeerList)
+        self.gui.displayStatus("Worker connected")
         
+        
+        self.nsmClientThread.started.connect(self.nsmClientWorker.run)
+
+        self.nsmClientThread.finished.connect(self.quit)
         self.nsmClientThread.start()
-        self.nsmClientThread.exec()
+        self.gui.displayStatus("Worker thread started")
+        #self.nsmClientThread.exec()
+        self.gui.displayStatus("Worker thread exec")
         qDebug("Worker in place, running...")
         
         
@@ -191,25 +285,34 @@ class Main(QObject):
     def nsmClientGet(self, ourNsmClient):
         self.ourNsmClient = ourNsmClient
         gui.testGui()
-    
-    def applyPort(self):
-        pass
+
     
     
     def quit(self):
-        self.title.setText("Quitte")
-        exit()
+        qDebug("quit called")
+        self.gui.displayStatus("Request to quit")
+        #self.nsmClientThread.exit()
+        #self.qtApp.exit()
+        return "Ready to quit"
 
         #self.title.setText(str(self.ourNsmClient.states.prettyNSMName))
 
 
 if __name__ == '__main__':
-            
+
     qtApp = QtWidgets.QApplication(sys.argv)
+    handler = SignalWakeupHandler(qtApp)
     nonNatUi = Gui(qtApp)
     nonNatUi.show()
     nonNat = Main(qtApp, nonNatUi)
+    
+#    timer = QTimer()
+#    timer.start(100)
+#    timer.timeout.connect(nonNatUi.showIncomeOsc())
 
+    """
+    signal.signal(signal.SIGTERM, Main.quit)
+    """
     ##################
     #Start everything
     qtApp.exec_()
